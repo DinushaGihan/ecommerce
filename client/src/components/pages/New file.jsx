2d71b72b1d9ef0eea250d76fc63c02bf4db1e372
DinushaGihan <116588856+DinushaGihan@users.noreply.github.com>
2023-04-10 22:37:53 +0530

    Added code

---
+++ client/src/components/pages/New file.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const New = () => {
+  return (
+    <div>
+      <div className="col-md-6 mx-auto">
+        <br />
+        <br />
+        <h1 className="col-md-8 mx-auto">Welcome to Home Page !</h1>
+        <br />
+        <p>
+          Online delivery service systems have become increasingly popular in
+          recent years due to the rise of e-commerce and the convenience they
+          provide. These systems enable customers to order goods and services
+          online and have them delivered directly to their doorstep, often
+          within a matter of hours. Online delivery service systems typically
+          involve a network of warehouses or distribution centers where products
+          are stored, and a fleet of delivery vehicles or couriers who transport
+          the products to customers.
+          <br />
+          <br /> In addition to the convenience of home delivery, these systems
+          often offer real-time tracking of deliveries, flexible delivery
+          options, and a wide range of products and services to choose from.
+          Online delivery service systems have revolutionized the way we shop
+          and have made it easier than ever for businesses to reach customers
+          around the world.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default New;
